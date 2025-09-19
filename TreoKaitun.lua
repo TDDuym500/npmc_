87a1f6c5eb8b240d15edfb6b81982d40695904b1
yeugaivotu <duyanhkhanh2018@gmail.com>
2025-09-19 20:02:31 +0700

    Update TreoKaitun.lua

--- TreoKaitun.lua
+++ TreoKaitun.lua
@@ -1,1 +1,4 @@
-
+getgenv().Key = "MARU-CI4O3-X7PH-XQEF-K3S3D-TNIRD"
+getgenv().id = "1310130803673661471"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
