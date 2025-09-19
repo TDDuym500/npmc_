39b544a5e831f60081b640523f17057199b2d5b9
yeugaivotu <duyanhkhanh2018@gmail.com>
2025-09-19 18:11:38 +0700

    Update TreoKaitun.lua

--- TreoKaitun.lua
+++ TreoKaitun.lua
@@ -1,3 +1,1 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().Key = "8b41f87176b72c074800ce39" 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
+
